a8a915c1cd2ffd4d96135acbe219758f71d45048
Romina Martin <alu0100696776@ull.edu.es>
2014-10-16 17:39:39 +0100

    Implementada la Resta de Complejos

--- imaginarios.rb
+++ imaginarios.rb
@@ -8,13 +8,20 @@ class Complejo
     @b = b	# Imaginario
     
   end
-  
+
   def to_s	# Devuelve una cadena con la representación del racional
     "#{@a} + #{@b}i"
   end
-  
+
   def suma (other)	# Devuelve un nuevo complejo que suma al objeto que invoca el que le pasan como parámetro
     @a = @a + other.a
     @b = @b + other.b
     self.to_s 	# Resultado como cadena
   end
+
+  def resta (other)	# Devuelve un nuevo complejo que resta al objeto que invoca el que le pasan como parámetro
+    @a = @a - other.a
+    @b = @b - other.b
+    self.to_s 	# Resultado como cadena
+  end
+end
--- tc_imaginarios.rb
+++ tc_imaginarios.rb
@@ -10,6 +10,7 @@ class Test_Complejos < Test::Unit::TestCase
 
   def test_simple	# Comprobar funcionalidad
      assert_equal("4 + 6i", @a.suma(@b).to_s)
+     assert_equal("-1 + -2i", @b.resta(@a).to_s)
   end
 
 end
